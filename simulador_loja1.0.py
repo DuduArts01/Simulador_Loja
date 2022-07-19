c8ef859c13a9ce76c6d06b1c9d9fc9472473f96b
StarlitVienna <pseudonothing@outlook.com>
2022-07-19 15:43:51 -0300

    Adicionado nova tela e top bar

--- simulador_loja1.0.py
+++ simulador_loja1.0.py
@@ -1,5 +1,6 @@
-from kivy.app import App
+from kivymd.app import MDApp as App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 from kivy.core.window import Window
 
 Window.size = (400, 800) 
@@ -8,10 +9,16 @@ class Gerenciador(ScreenManager):
     pass
 
 class Menu_Principal(Screen):
+    def mudar_tela(self):
+        self.parent.transition.direction = 'left'
+        self.parent.current = "menu2"
     pass
 
 class Simulador_loja(App):
     def build(self):
-        return
+        self.theme_cls.theme_style = 'Dark'
+        kv = Builder.load_file("./tela_inicial.kv")
+        return kv
+    pass
 
 Simulador_loja().run()
